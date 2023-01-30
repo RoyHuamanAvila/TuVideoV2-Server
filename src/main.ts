6a1e1f16d4c76366be6dd95cf54edd30bcc8ff0f
Roy <rhavila789@gmail.com>
2023-01-30 17:45:20 -0500

    feat: port env

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
 }
 bootstrap();
